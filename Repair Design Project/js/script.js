8d5dc40512f319dd0f99ec75659b6c98a9964809
AenSaidhe <said123@outlook.com>
2020-05-29 15:29:05 +0300

    Created RDP

---
+++ Repair Design Project/js/script.js
@@ -0,0 +1,11 @@
+let mySwiper = new Swiper('.swiper-container', {
+  speed: 500,
+  spaceBetween: 100,
+  effect: 'cube', 
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets'
+  }
+})
+
